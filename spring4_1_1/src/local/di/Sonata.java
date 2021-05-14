386f67cbdb67d0c824bce6ef628e1b7cf04f879c
nicehappy0919 <candysy2014@naver.com>
2021-05-14 18:19:21 +0900

    생성자 객체 주입법 코드 실습3

--- spring4_1_1/src/local/di/Sonata.java
+++ spring4_1_1/src/local/di/Sonata.java
@@ -1,6 +1,27 @@
 package local.di;
 
+//import org.springframework.context.annotation.Scope;
+
+//@Scope("singletone")
 public class Sonata {
+	public String getCarColor() {
+		return carColor;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+	public int getWheelNum() {
+		return wheelNum;
+	}
+	public void setWheelNum(int wheelNum) {
+		this.wheelNum = wheelNum;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 	private String carColor = null;
 	private int wheelNum = 0;
 	private int speed = 0;
@@ -10,12 +31,12 @@ public class Sonata {
 		this.wheelNum = wheelNum;
 	}
 	public Sonata(String carColor
-				, int wheelNum
-				, int speed) {
+			    , int wheelNum
+			    , int speed) {
 		this.carColor = carColor;
 		this.wheelNum = wheelNum;
 		this.speed = speed;
-	}
+	}	
 	@Override
 	public String toString() {
 		return "자동차 정보 출력하기";
--- spring4_1_1/src/local/di/SonataApx.java
+++ spring4_1_1/src/local/di/SonataApx.java
@@ -4,16 +4,31 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SonataApx {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Sonata herCar = new Sonata();
 		ApplicationContext context = 
-				new ClassPathXmlApplicationContext("sonataBean.xml");
+				new ClassPathXmlApplicationContext("local\\di\\sonataBean.xml");
 		Sonata myCar = null;
 		myCar = (Sonata)context.getBean("myCar");
 		System.out.println(myCar.toString());
+		Sonata yourCar = null;
+		yourCar = (Sonata)context.getBean("yourCar");
+		System.out.println(yourCar.toString());
+//		System.out.println(himCar.speed);
+		System.out.println("yourCar의 현재 속도는 "+yourCar.getSpeed()+" 입니다.");
+		Sonata meCar = null;
+		meCar = (Sonata)context.getBean("meCar");
+		System.out.println(meCar.toString());
+//		System.out.println(himCar.speed);
+		System.out.println("meCar의 현재 속도는 "+meCar.getSpeed()+" 입니다.");
 		Sonata himCar = null;
-		myCar = (Sonata)context.getBean("himCar");
-		System.out.println(myCar.toString());
-		System.out.println("himCar의 현재 속도는 "+himCar.getSpeed() + " 입니다.");
+		himCar = (Sonata)context.getBean("himCar");
+		System.out.println(himCar.toString());
+//		System.out.println(himCar.speed);
+		System.out.println("himCar의 현재 속도는 "+himCar.getSpeed()+" 입니다.");
+		
 	}
+
 }
